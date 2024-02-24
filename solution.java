24376a6cbfd3e6bcbd6c9691279a34918f648050
Kooreis <kooreis.official@gmail.com>
2024-02-24 09:13:11 -0600

    This update introduces the Block class, which represents a block of memory. Each block has a size and a byte array representing the memory of the block.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a simple implementation of a memory allocator in Java. This implementation uses a LinkedList to keep track of free blocks of memory. When a block is allocated, it is removed from the list of free blocks. When a block is freed, it is added back to the list of free blocks.
-
-```java
 import java.util.LinkedList;
 
 class Block {
@@ -12,39 +9,3 @@ class Block {
         this.memory = new byte[size];
     }
 }
-
-class MemoryAllocator {
-    LinkedList<Block> freeBlocks;
-
-    public MemoryAllocator(int totalSize) {
-        freeBlocks = new LinkedList<>();
-        freeBlocks.add(new Block(totalSize));
-    }
-
-    public Block allocate(int size) {
-        for (Block block : freeBlocks) {
-            if (block.size >= size) {
-                freeBlocks.remove(block);
-                return block;
-            }
-        }
-        return null;
-    }
-
-    public void free(Block block) {
-        freeBlocks.add(block);
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        MemoryAllocator allocator = new MemoryAllocator(1000);
-        Block block1 = allocator.allocate(200);
-        Block block2 = allocator.allocate(300);
-        allocator.free(block1);
-        Block block3 = allocator.allocate(100);
-    }
-}
-```
-
-This is a very simple implementation and does not handle cases where the requested size is larger than any available block. In a real-world memory allocator, you would need to handle this case, as well as cases where the memory is fragmented (i.e., the free memory is not all in one contiguous block).
