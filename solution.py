aeacc36a693379124a9806563b5152ae3d381f4f
Kooreis <kooreis.official@gmail.com>
2024-02-25 08:13:11 -0600

    Added the allocate method to the MemoryAllocator class. This method finds the first block of free memory of the requested size, marks it as allocated, and returns the starting index of the block. If no block of sufficient size is found, it returns -1.

--- solution.py
+++ solution.py
@@ -1,4 +1,8 @@
-class MemoryAllocator:
-    def __init__(self, size):
-        self.memory = [0] * size
-        self.blocks = {}
+def allocate(self, size):
+        for i in range(len(self.memory)):
+            if self.memory[i:i+size] == [0]*size:
+                self.blocks[i] = size
+                for j in range(size):
+                    self.memory[i+j] = 1
+                return i
+        return -1
